20daddad63ae6d1a5fa9298232cc2b71addd7d4e
Ain Tohvri <ain@flashbit.net>
2014-06-15 23:28:38 +0200

    Add basic initialisation and mock implementation

---
+++ src/perimeeter.js
@@ -0,0 +1,103 @@
+/*! perimeeter.js - v0.0.1 - 2014-06-14
+ * https://github.com/ain/perimeeter.js
+ * Copyright © 2014 Ain Tohvri; Licensed GPL */
+
+/* exported Perimeeter */
+/* global navigator */
+/* global Geocoder */
+/* global signals */
+var Perimeeter = (function() {
+
+  'use strict';
+
+  var Signal = signals.Signal,
+    coords = null,
+    defaults = {
+      radius: 0,
+      capabilityDetection: false
+    },
+    signalCollection = {
+      positioned: null,
+      approximated: null
+    };
+
+
+  function parseRadius(radius) {
+    var parsedRadius = parseFloat(radius);
+    if (/^\d+$/.test(parsedRadius)) {
+      return parsedRadius;
+    }
+    throw new TypeError('Invalid option for radius! Number expected.');
+  }
+
+  function parseOption(option, value) {
+    if (option === 'radius') {
+      return parseRadius(value);
+    }
+    return value;
+  }
+
+  function parseOptions(options) {
+    if (typeof options !== 'object') {
+      throw new TypeError('Invalid format for options! Object expected.');
+    }
+    var parsedOptions = defaults;
+    for (var option in defaults) {
+      if (options[option] !== undefined) {
+        parsedOptions[option] = parseOption(option, options[option]);
+      }
+    }
+    return parsedOptions;
+  }
+
+  function addSignals() {
+    signalCollection.positioned = new Signal();
+    signalCollection.approximated = new Signal();
+  }
+
+  function Perimeeter(options) {
+    this.options = options ? parseOptions(options) : defaults;
+    addSignals();
+  }
+
+  Perimeeter.prototype.getSignals = function() {
+    return signalCollection;
+  };
+
+  /*Perimeeter.prototype.addSignals = function() {
+    signals.positioned = new Signal();
+    signals.approximated = new Signal();
+  };*/
+
+  Perimeeter.prototype.getRadius = function() {
+    return this.options.radius;
+  };
+
+  Perimeeter.prototype.getCoordinates = function() {
+    return coords;
+  };
+
+  function handleCurrentPositionSuccess(position) {
+    coords = position.coords;
+    signalCollection.positioned.dispatch(coords);
+  }
+
+  function handleCurrentPositionFailure() {
+    var error = new Error('Failed to get current position!');
+    signalCollection.positioned.dispatch(error);
+    throw error;
+  }
+
+  Perimeeter.prototype.position = function() {
+    navigator.geolocation.getCurrentPosition(handleCurrentPositionSuccess, handleCurrentPositionFailure);
+    //var geocoder = new Geocoder();
+    //geocoder.geocode();
+  };
+
+  Perimeeter.isCapablePlatform = function() {
+    return !!navigator.geolocation;
+  };
+
+  return Perimeeter;
+
+})();
---
+++ test/spec/test.js
@@ -0,0 +1,100 @@
+/*! perimeeter.js - v0.0.1 - 2014-06-14
+ * * https://github.com/ain/perimeeter.js
+ * * Copyright © 2014 Ain Tohvri; Licensed GPL */
+'use strict';
+/* jshint undef: false */
+(function () {
+  describe('perimeeter', function () {
+
+    var perimeeter,
+      geolocation;
+
+    describe('init', function () {
+
+      beforeEach(function() {
+        perimeeter = new Perimeeter();
+      });
+
+      it('expected to construct object', function () {
+        return expect(perimeeter).to.be.an.object;
+      });
+
+      it('expected to default radius to 0', function() {
+        return expect(perimeeter.getRadius()).to.equal(0);
+      });
+
+      it('expected to set radius from constructor', function() {
+        perimeeter = new Perimeeter({radius: 20});
+        return expect(perimeeter.getRadius()).to.equal(20);
+      });
+
+      it('expected to throw type error on String input', function() {
+        function test() {
+          perimeeter = new Perimeeter('ain');
+        }
+        return expect(test).to.throw(TypeError);
+      });
+    });
+
+    describe('capabilities', function() {
+      it('expected to have no support for geolocation without mock extension', function() {
+        return expect(Perimeeter.isCapablePlatform()).to.be.false;
+      });
+
+      it('expected to have support for geolocation with mock extension', function() {
+        new Geolocation();
+        return expect(Perimeeter.isCapablePlatform()).to.be.true;
+      });
+    });
+
+    describe('geolocation mock extension', function() {
+
+      var latitude = 53,
+        longitude = 9,
+        coords;
+
+      beforeEach(function(done) {
+        new Geolocation(latitude, longitude);
+        navigator.geolocation.getCurrentPosition(function(position) {
+          coords = position.coords;
+          done();
+        });
+      });
+
+      it('expected to get coordinates object', function() {
+        return expect(coords).to.be.object;
+      });
+
+      it('expected to get predefined latitude and longitude', function() {
+        return expect(coords).to.deep.equal({ latitude: latitude, longitude: longitude });
+      });
+    });
+
+    describe('position', function() {
+
+      beforeEach(function(done) {
+        geolocation = new Geolocation();
+        perimeeter = new Perimeeter();
+        perimeeter.getSignals().positioned.add(function(position) {
+          done();
+        });
+        perimeeter.position();
+      });
+
+      it('expected to set position coordinates object', function() {
+        return expect(perimeeter.getCoordinates()).to.be.object;
+      });
+
+      it('expected to set position coordinates from mock extension', function() {
+        var coords = {
+          latitude: geolocation.latitude,
+          longitude: geolocation.longitude
+        };
+        return expect(perimeeter.getCoordinates()).to.deep.equal(coords);
+      });
+
+
+
+    });
+  });
+})();
